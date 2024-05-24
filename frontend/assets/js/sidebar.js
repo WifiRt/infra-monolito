4148c176e5643db3a6636c2341287dd857bba83b
WifiRt <nalberth.bz@gmail.com>
2024-05-23 21:36:50 -0300

    home-page

---
+++ frontend/assets/js/sidebar.js
@@ -0,0 +1,4 @@
+const sidebar = document.querySelector('.sidebar');
+document.querySelector('.menu-sidebar').onclick = () => sidebar.style.display = 'flex'
+document.querySelector('.close').onclick = () => sidebar.style.display = 'none'
+
